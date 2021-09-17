b9b6ff0e2fe2f870d04ffa32752d0d4a62e1c8b7
DESKTOP-HBBKP3F\citydo <1270985196@qq.com>
2021-09-17 18:08:10 +0800

    地图开发

--- src/views/components/Map/number_option.js
+++ src/views/components/Map/number_option.js
@@ -9,48 +9,49 @@ export default function getOption(data) {
   data.sort(NumDescSort);
 
   return {
-    visualMap: {
-      show: false,
-      type: 'continuous',
-      min: 0,
-      max: data[0].value,
-      seriesIndex: [0, 2],
-      dimension: 0,
-      realtime: false,
-      left: 0,
-      itemWidth: 18,
-      itemHeight: 100,
-      calculable: true,
-      inRange: {
-        color: ['#9BC4FF', '#3B6DE6'],
-        symbolSize: [100, 100]
+    tooltip: {
+      trigger: "item"
+    },
+    visualMap: { // 视觉映射组件
+      show: true,
+      type: 'continuous', // 连续型视觉映射组件
+      min: 0, // 组件的允许的最小值
+      max: data[0].value, // 组件的允许的最大值
+      seriesIndex: [0, 2], // 指定取哪个系列的数据，即哪个系列的 series.data。
+      realtime: false, // 拖拽时，是否实时更新。如果为ture则拖拽手柄过程中实时更新图表视图。如果为false则拖拽结束时，才更新视图。
+      right: 44, // visualMap 组件离容器左侧的距离。
+      itemWidth: 12, // 图形的宽度，即长条的宽度。
+      itemHeight: 86, // 图形的高度，即长条的高度。
+      calculable: true, // 是否显示拖拽用的手柄（手柄能拖拽调整选中范围）。
+      inRange: {  // 定义 在选中范围中 的视觉元素。（用户可以和 visualMap 组件交互，用鼠标或触摸选择范围）
+        color: ['#9BC4FF', '#3B6DE6'], // 图元的颜色。
+        symbolSize: [100, 100] // 图元的大小。
       },
-      outOfRange: {
-        color: ['#eeeeee'],
-        symbolSize: [100, 100]
-      },
+      textStyle: {
+        color: '#ffffff'
+      }
     },
     series: [
       {
-        name: "占比",
+        name: "分布数量",
         type: "map",
         mapType: "浙江省",
         // left: '100',
         // width: '60%',
         roam: false,
-        mapValueCalculation: "sum",
+        mapValueCalculation: "sum", // 多个拥有相同地图类型的系列会使用同一个地图展现，如果多个系列都在同一个区域有值，ECharts 会对这些值统计得到一个数据。这个配置项就是用于配置统计的方式
         zoom: 1.2,
-        selectedMode: false,
-        showLegendSymbol: false,
+        selectedMode: false, // 选中模式，表示是否支持多个选中，默认关闭，支持布尔值和字符串，字符串取值可选'single'表示单选，或者'multiple'表示多选。
+        showLegendSymbol: false, // 在图例相应区域显示图例的颜色标识（系列标识的小圆点），存在 legend 组件时生效。
         label: {
           normal: {
             textStyle: {
-              color: '#666'
+              color: '#ffffff'
             }
           },
           emphasis: {
             textStyle: {
-              color: '#234EA5'
+              color: '#ffffff'
             }
           }
         },
---
+++ src/views/components/Map/spot_option.js
@@ -0,0 +1,207 @@
+export default function getSpotOption (data) {
+  return {
+    color: ['#1E913F',''],
+    tooltip: {
+      trigger: "item"
+    },
+    geo: {
+      map: '浙江省', // 使用 registerMap 注册的地图名称。
+      show: true, // 是否显示地理坐标系组件。
+      roam: false, // 是否开启鼠标缩放和平移漫游。默认不开启。如果只想要开启缩放或者平移，
+      zoom: 1.2,
+      itemStyle: { //控制镇名文字显示
+        normal: {
+          areaColor: '#213E5F', //区域背景颜色
+          borderColor: '#79C0F6', //描边颜色
+          label: {
+            show: true,
+            color: '#FFFFFF',
+            fontSize: 20,
+            fontFamily: 'Microsoft YaHei',
+          },
+          itemStyle: {
+            color: '#4979A4',
+          },
+        },
+        emphasis: { //高亮状态下的样式
+          areaColor: '#4979A4', //区域背景颜色
+          label: {
+            show: true,
+            color: '#FFFFFF',
+            fontSize: 20,
+            fontFamily: 'Microsoft YaHei',
+            shadowColor: 'rgba(0, 0, 0, 0.36)',
+            shadowBlur: 4,
+            shadowOffsetY: 4,
+          },
+        }
+      },
+      data: []
+    },
+    series: [
+      {
+        name: '浙农游',
+        type: 'map',
+        mapType: '浙江省',
+        roam: false,
+        zoom: 1.2,
+        itemStyle: { //控制镇名文字显示
+          normal: {
+            areaColor: '#213E5F', //区域背景颜色
+            borderColor: '#79C0F6', //描边颜色
+            label: {
+              show: true,
+              color: '#FFFFFF',
+              fontSize: 20,
+              fontFamily: 'Microsoft YaHei',
+            },
+            itemStyle: {
+              color: '#4979A4',
+            },
+          },
+          emphasis: { //高亮状态下的样式
+            areaColor: '#4979A4', //区域背景颜色
+            label: {
+              show: true,
+              color: '#FFFFFF',
+              fontSize: 20,
+              fontFamily: 'Microsoft YaHei',
+              shadowColor: 'rgba(0, 0, 0, 0.36)',
+              shadowBlur: 4,
+              shadowOffsetY: 4,
+            },
+          }
+        },
+        data: []
+      },
+      {
+        name: '24节气村',
+        type: 'scatter', // 散点（气泡）图。直角坐标系上的散点图可以用来展现数据的 x，y 之间的关系，如果数据项有多个维度，其它维度的值可以通过不同大小的 symbol 展现成气泡图，也可以用颜色来表现。这些可以配合 visualMap 组件完成。
+        // 可以应用在直角坐标系，极坐标系，地理坐标系上。
+        coordinateSystem: 'geo', // 该系列使用的坐标系，可选： 'cartesian2d'：二维的直角坐标系 ； 'polar'：使用极坐标系； 'geo'：使用地理坐标系，通过 geoIndex 指定相应的地理坐标系组件。
+        data: data.data1, // 系列中的数据内容数组。数组项通常为具体的数据项。 // 维度X   维度Y   其他维度 ...
+        colorBy: 'series',
+        symbol: 'circle',
+        symbolSize: 10,
+        symbolKeepAspect: true, // 保持缩放比例
+        label: { //标签：图形上的文本标签，可用于说明图形的一些数据信息，比如值，名称等。
+          normal: {
+            formatter: '{a}', // 标签内容格式器，支持字符串模板和回调函数两种形式，字符串模板与回调函数返回的字符串均支持用 \n 换行。
+            //字符串模板 模板变量有：
+            // {a}：系列名。
+            // {b}：数据名。
+            // {c}：数据值。
+            // {@xxx}：数据中名为 'xxx' 的维度的值，如 {@product} 表示名为 'product' 的维度的值。
+            // {@[n]}：数据中维度 n 的值，如 {@[3]} 表示维度 3 的值，从 0 开始计数。 {b}: {@score}
+            position: 'right',
+            show: false // 是否常显
+          },
+          emphasis: {
+            show: false
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#2D7EE7'
+          }
+        }
+      },
+      {
+        name: '农业文化遗产地',
+        type: 'scatter', // 散点（气泡）图。直角坐标系上的散点图可以用来展现数据的 x，y 之间的关系，如果数据项有多个维度，其它维度的值可以通过不同大小的 symbol 展现成气泡图，也可以用颜色来表现。这些可以配合 visualMap 组件完成。
+        // 可以应用在直角坐标系，极坐标系，地理坐标系上。
+        coordinateSystem: 'geo', // 该系列使用的坐标系，可选： 'cartesian2d'：二维的直角坐标系 ； 'polar'：使用极坐标系； 'geo'：使用地理坐标系，通过 geoIndex 指定相应的地理坐标系组件。
+        data: data.data2, // 系列中的数据内容数组。数组项通常为具体的数据项。 // 维度X   维度Y   其他维度 ...
+        colorBy: 'series',
+        symbol: 'circle',
+        symbolSize: 10,
+        symbolKeepAspect: true, // 保持缩放比例
+        label: { //标签：图形上的文本标签，可用于说明图形的一些数据信息，比如值，名称等。
+          normal: {
+            formatter: '{a}', // 标签内容格式器，支持字符串模板和回调函数两种形式，字符串模板与回调函数返回的字符串均支持用 \n 换行。
+            //字符串模板 模板变量有：
+            // {a}：系列名。
+            // {b}：数据名。
+            // {c}：数据值。
+            // {@xxx}：数据中名为 'xxx' 的维度的值，如 {@product} 表示名为 'product' 的维度的值。
+            // {@[n]}：数据中维度 n 的值，如 {@[3]} 表示维度 3 的值，从 0 开始计数。 {b}: {@score}
+            position: 'right',
+            show: false // 是否常显
+          },
+          emphasis: {
+            show: false
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#1E913F'
+          }
+        }
+      },
+      {
+        name: '历史文化重点保护村',
+        type: 'scatter', // 散点（气泡）图。直角坐标系上的散点图可以用来展现数据的 x，y 之间的关系，如果数据项有多个维度，其它维度的值可以通过不同大小的 symbol 展现成气泡图，也可以用颜色来表现。这些可以配合 visualMap 组件完成。
+        // 可以应用在直角坐标系，极坐标系，地理坐标系上。
+        coordinateSystem: 'geo', // 该系列使用的坐标系，可选： 'cartesian2d'：二维的直角坐标系 ； 'polar'：使用极坐标系； 'geo'：使用地理坐标系，通过 geoIndex 指定相应的地理坐标系组件。
+        data: data.data3, // 系列中的数据内容数组。数组项通常为具体的数据项。 // 维度X   维度Y   其他维度 ...
+        colorBy: 'series',
+        symbol: 'circle',
+        symbolSize: 10,
+        symbolKeepAspect: true, // 保持缩放比例
+        label: { //标签：图形上的文本标签，可用于说明图形的一些数据信息，比如值，名称等。
+          normal: {
+            formatter: '{a}', // 标签内容格式器，支持字符串模板和回调函数两种形式，字符串模板与回调函数返回的字符串均支持用 \n 换行。
+            //字符串模板 模板变量有：
+            // {a}：系列名。
+            // {b}：数据名。
+            // {c}：数据值。
+            // {@xxx}：数据中名为 'xxx' 的维度的值，如 {@product} 表示名为 'product' 的维度的值。
+            // {@[n]}：数据中维度 n 的值，如 {@[3]} 表示维度 3 的值，从 0 开始计数。 {b}: {@score}
+            position: 'right',
+            show: false // 是否常显
+          },
+          emphasis: {
+            show: false
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#F6B980'
+          }
+        }
+      },
+      {
+        name: '历史文化重点保护村',
+        type: 'scatter', // 散点（气泡）图。直角坐标系上的散点图可以用来展现数据的 x，y 之间的关系，如果数据项有多个维度，其它维度的值可以通过不同大小的 symbol 展现成气泡图，也可以用颜色来表现。这些可以配合 visualMap 组件完成。
+        // 可以应用在直角坐标系，极坐标系，地理坐标系上。
+        coordinateSystem: 'geo', // 该系列使用的坐标系，可选： 'cartesian2d'：二维的直角坐标系 ； 'polar'：使用极坐标系； 'geo'：使用地理坐标系，通过 geoIndex 指定相应的地理坐标系组件。
+        data: data.data4, // 系列中的数据内容数组。数组项通常为具体的数据项。 // 维度X   维度Y   其他维度 ...
+        colorBy: 'series',
+        symbol: 'circle',
+        symbolSize: 10,
+        symbolKeepAspect: true, // 保持缩放比例
+        label: { //标签：图形上的文本标签，可用于说明图形的一些数据信息，比如值，名称等。
+          normal: {
+            formatter: '{a}', // 标签内容格式器，支持字符串模板和回调函数两种形式，字符串模板与回调函数返回的字符串均支持用 \n 换行。
+            //字符串模板 模板变量有：
+            // {a}：系列名。
+            // {b}：数据名。
+            // {c}：数据值。
+            // {@xxx}：数据中名为 'xxx' 的维度的值，如 {@product} 表示名为 'product' 的维度的值。
+            // {@[n]}：数据中维度 n 的值，如 {@[3]} 表示维度 3 的值，从 0 开始计数。 {b}: {@score}
+            position: 'right',
+            show: false // 是否常显
+          },
+          emphasis: {
+            show: false
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#00DCA6'
+          }
+        }
+      },
+    ]
+  }
+}
